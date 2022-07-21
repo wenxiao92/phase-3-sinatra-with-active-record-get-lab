0fdb1ee25828aa0a7361848ca77fe376955d4c70
Wen Xiao <wennstyle@gmail.com>
2022-07-20 22:35:50 -0400

    commit tree ead6731c6e850e82d29997f13a32be79dd74277e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,28 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
+
   # add routes
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    bakedGood = BakedGood.all.order("price DESC")
+    #bakedGood.order(:price).to_json
+    bakedGood.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    bakedGood = BakedGood.all.order("price DESC").first
+    #bakedGood.order(:price).to_json
+    bakedGood.to_json
+  end
 
 end
